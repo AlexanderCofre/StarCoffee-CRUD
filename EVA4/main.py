b2318c470d8a9c0cde89dc2464ba11ea0d3fb4f6
AlexanderDev <brioneselias2024@gmail.com>
2025-02-28 23:50:29 -0300

    Punto de entrada del programa

---
+++ EVA4/main.py
@@ -0,0 +1,16 @@
+# Importa la función login desde el módulo menu
+from MENU.menu import login
+
+# Asegúrate de tener instaladas las librerías necesarias
+# Instalar la libreria tabulate
+# pip install tabulate
+# Instalar el conector de MySQL para Python
+# pip install mysql-connector-python
+
+# Credenciales de prueba
+# nombre: elias
+# contraseña: asdf
+
+# Inicia el programa llamando a la función login
+if __name__ == "__main__":
+    login()
